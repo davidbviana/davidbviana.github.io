e81a15b6a647e624b3c6d8b0519a3c4c9e0a6a88
David Viana <me@davidviana.com>
2023-08-07 14:20:09 +0100

    Add initial logic, dummy data

---
+++ main.py
@@ -0,0 +1,25 @@
+import csv
+from jinja2 import Environment, FileSystemLoader
+
+
+def read_csv(file_path):
+    with open(file_path, "r", newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = [row for row in reader]
+    return data
+
+
+def generate_html(data):
+    env = Environment(loader=FileSystemLoader("."))
+    template = env.get_template("template.html")
+    return template.render(data=data)
+
+
+if __name__ == "__main__":
+    data = read_csv("data.csv")
+    html_content = generate_html(data)
+
+    with open(output_file := "index.html", "w") as htmlfile:
+        htmlfile.write(html_content)
+
+    print(f"{output_file} generated successfully!")
